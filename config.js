e52973d5f6e22ed4ee5833cbe20a29df0ab1d213
monomilo <90976470+monomilo@users.noreply.github.com>
2025-11-09 16:02:35 +0630

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["Milo", ""], 
   mongodbUri : "mongodb+srv://milo:milo@123@discordbot.afyhoaw.mongodb.net",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
+  spotifyClientId : "922436dfc2364fb1bad04e478ccf874a",
+  spotifyClientSecret : "0b444b694119409db21d98e78394fd5f",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "Milo",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
