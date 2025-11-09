f90e0de89c733fdf3ee5cd7d2e1baf3d27bba81e
monomilo <90976470+monomilo@users.noreply.github.com>
2025-11-09 16:00:41 +0630

    Update config.js

--- config.js
+++ config.js
@@ -4,8 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://milo:milo@123@discordbot.afyhoaw.mongodb.net/?appName=Discordbot
-",
+  mongodbUri : "mongodb+srv://milo:milo@123@discordbot.afyhoaw.mongodb.net",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
